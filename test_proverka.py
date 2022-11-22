33aaee33165cf5c2937959528b3a64c4001b0c75
Mikhail <nikiforov1601@gmail.com>
2022-11-22 07:57:01 +0300

    перенесены файлы из основной директории, скорректирован файл проверка

--- test_proverka.py
+++ test_proverka.py
@@ -1,4 +1,9 @@
-from translatepy import Translator
+server1 = False
+server2 = False
+server3 = None
 
-translator = Translator()
-print(translator.translate("Hello world", "ru"))
+result = server1 or server2 or server3
+result2 = server1 and server2 and server3
+
+print(result)
+print(result2)
