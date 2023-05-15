ce156076a6900ad6594370854c3f14ea1e6d0090
Misha1601 <nikiforov1601@gmail.com>
2023-05-15 14:30:50 +0300

    Рабочий вариант для слов

--- bot/create_offer_and_send3.py
+++ bot/create_offer_and_send3.py
@@ -1,36 +1,15 @@
 from aiogram import types
-import os
-import random
-import json
 from main import bot
 from bot.keyboards import buttons_answer, buttons_no_menu
 from app.generator import Generate
 from app.translate import Translate
 from bot.data import del_old_messege, save_info_messege, statistics
 
-file_list = os.getcwd()
-
-if os.name == 'posix':
-    json_files = [file_list+'/app/word_collection1/word_3-5.json'
-#                   file_list+'/app/word_collection1/word_gl_fras.json',
-#                   file_list+'/app/word_collection1/word_gl_modl.json',
-#                   file_list+'/app/word_collection1/word_gl_nepr.json',
-#                   file_list+'/app/word_collection1/word_gl_pr.json'
-                  ]
-else:
-    json_files = [file_list+'\\app\word_collection1\word_3-5.json'
-#                   file_list+'\\app\word_collection1\word_gl_fras.json',
-#                   file_list+'\\app\word_collection1\word_gl_modl.json',
-#                   file_list+'\\app\word_collection1\word_gl_nepr.json',
-#                   file_list+'\\app\word_collection1\word_gl_pr.json'
-                    ]
-with open(json_files[0], 'r', encoding='utf-8') as file:
-    data1 = json.load(file)
 
 def generate_offer(mes_or_key):
     import bot.data as data
-    word = random.choice(list(data1.keys()))
-    word_translate = data1[word]
+    word = Generate().offer()
+    word_translate = Translate(word=word).perevod()
     if str(mes_or_key).isnumeric():
         data.WORD[mes_or_key] = [word, word_translate]
     else:
@@ -40,7 +19,8 @@ def generate_offer(mes_or_key):
 # вывести в отдельную функцию генерацию ответа offer_and_translate
 def offer_and_translate(mes_or_key):
     word = generate_offer(mes_or_key)
-    offer = f"{word[0]}\n ||{word[1]}||"
+    offer = f"{word[0]}\n ||Google \- {word[1][0]}||\n||Yandex \- {word[1][1]}||\n||Deepl \- {word[1][2]}||"
+    # offer = "hello world"
     return offer
 
 
@@ -64,6 +44,3 @@ async def game(message_or_key): # сделать принимающим 1 пар
     await save_info_messege(mes)
     await save_info_messege(mes1)
     await statistics(message=mes, quest=True)
-
-if __name__ == "__main__":
-    print(offer_and_translate('123'))
